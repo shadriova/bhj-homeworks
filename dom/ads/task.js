2ca70ee9c2404653d8d0284e860b72e5938ebfab
Shadrinova Diana <shadrinovva@gmail.com>
2024-10-20 16:33:19 +0300

    2 задание готово

---
+++ dom/ads/task.js
@@ -0,0 +1,15 @@
+function startRotator(rotator) {
+    const cases = rotator.querySelectorAll('.rotator__case');
+    let currentIndex = 0;
+
+    setInterval(() => {
+        cases[currentIndex].classList.remove('rotator__case_active');
+        currentIndex = (currentIndex + 1) % cases.length;
+
+        cases[currentIndex].classList.add('rotator__case_active');
+    }, 1000); 
+}
+
+document.querySelectorAll('.rotator').forEach((rotator) => {
+    startRotator(rotator);
+});
